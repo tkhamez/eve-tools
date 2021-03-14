3d125353d22491376a9d58b1a554ec2028b752d5
Tian Khamez <tkhamez@gmail.com>
2021-03-14 17:23:28 +0100

    Add column reward/m3 for first table, remove order function from calculated reward columns, some JS cleanups.

--- src/main/resources/static/assets/scripts.js
+++ src/main/resources/static/assets/scripts.js
@@ -23,14 +23,15 @@ window.$(function() {
     }, 300000); // 5 minutes
 });
 
-var Assets = (function($, undefined) { /* jshint ignore: line*/
+window.Assets = (function($) {
 
+    // noinspection JSUnusedGlobalSymbols
     return {
-
         toggleContainer: function(id) {
-            $('.location-'+id).toggle();
+            const $location = $('.location-'+id);
+            $location.toggle();
 
-            if (! $('.location-'+id).is(':visible')) {
+            if (!$location.is(':visible')) {
                 $('.parent-location-'+id).hide();
             }
         },
@@ -46,41 +47,42 @@ var Assets = (function($, undefined) { /* jshint ignore: line*/
     };
 })(window.$);
 
-
-var CorpCourierContracts = (function($, undefined) {
+window.CorpCourierContracts = (function($) {
 
     function calcSums(table) {
-        var volume = 0;
-        var collateral = 0;
-        var reward = 0;
-        var calcReward = 0;
+        let volume = 0;
+        let collateral = 0;
+        let reward = 0;
+        let calcReward = 0;
 
-        var tc = '.' + table;
+        const column = '.' + table;
 
-        $(tc + ' .selected .volume').each(function() {
+        $(column + ' .selected .volume').each(function() {
             volume += parseFloat($(this).data('value'));
         });
-        $(tc + ' .selected .collateral').each(function() {
+        $(column + ' .selected .collateral').each(function() {
             collateral += parseFloat($(this).data('value'));
         });
-        $(tc + ' .selected .reward').each(function() {
+        $(column + ' .selected .reward').each(function() {
             reward += parseFloat($(this).data('value'));
         });
-        $(tc + ' .selected .calc-reward').each(function() {
+        $(column + ' .selected .calc-reward').each(function() {
         	calcReward += parseFloat($(this).data('value'));
         });
 
-        $(tc + ' .sum .volume').text(
-    		volume.toLocaleString('en-GB', { minimumFractionDigits: 2, maximumFractionDigits: 2 }));
-        $(tc + ' .sum .collateral').text(Math.round(collateral).toLocaleString('en-GB'));
-        $(tc + ' .sum .reward').text(Math.round(reward).toLocaleString('en-GB'));
-        $(tc + ' .sum .calc-reward').text(Math.round(calcReward).toLocaleString('en-GB'));
+        $(column + ' .sum .volume').text(volume.toLocaleString(
+            'en-GB',
+            { minimumFractionDigits: 2, maximumFractionDigits: 2 }
+        ));
+        $(column + ' .sum .collateral').text(Math.round(collateral).toLocaleString('en-GB'));
+        $(column + ' .sum .reward').text(Math.round(reward).toLocaleString('en-GB'));
+        $(column + ' .sum .calc-reward').text(Math.round(calcReward).toLocaleString('en-GB'));
     }
 
     function calcReward() {
 
     	function getSystem(location) {
-    	    for (var i = 0; i < Route.graph.length; i++) {
+    	    for (let i = 0; i < Route.graph.length; i++) {
     	        if (location.indexOf(Route.graph[i].name) === 0) {
     	            return Route.graph[i].name;
     	        }
@@ -93,35 +95,35 @@ var CorpCourierContracts = (function($, undefined) {
             } else if (endLoc === 'Mendori') {
                 endLoc = 'Rahadalon';
             }
-            var startSys = Route.findSystem(startLoc);
-            var endSys = Route.findSystem(endLoc);
+            const startSys = Route.findSystem(startLoc);
+            const endSys = Route.findSystem(endLoc);
             return Route.calculateRoute(startSys, endSys, 10);
         }
 
-    	var bltMaxVolume    = 320000;
-    	var bltMaxCollat    = 3000000000;
-    	var baseFee         = 0;
-    	var cynoFee         = 5000000; // per jump
-    	var distanceFee     = 8000000; // per ly
+        const bltMaxVolume = 320000;
+        const bltMaxCollateral = 3000000000;
+        const baseFee = 0;
+        const cynoFee = 5000000; // per jump
+        const distanceFee = 8000000; // per ly
 
     	$('tbody .reward').each(function() {
-    		var $start = $(this).closest('tr').find('.start');
-    		var $end = $(this).closest('tr').find('.end');
-    		var $volumeCell = $(this).closest('tr').find('.volume');
-    		var $collatCell = $(this).closest('tr').find('.collateral');
-    		var $targetCell = $(this).closest('tr').find('.calc-reward');
+            const $start = $(this).closest('tr').find('.start');
+            const $end = $(this).closest('tr').find('.end');
+            const $volumeCell = $(this).closest('tr').find('.volume');
+            const $collateralCell = $(this).closest('tr').find('.collateral');
+            const $targetCell = $(this).closest('tr').find('.calc-reward');
 
-            var volume = parseFloat($volumeCell.data('value'));
-            var collateral = parseFloat($collatCell.data('value'));
+            const volume = parseFloat($volumeCell.data('value'));
+            const collateral = parseFloat($collateralCell.data('value'));
 
-            var startLoc = getSystem($start.text());
-            var endLoc = getSystem($end.text());
-            var route = calculateRoute(startLoc, endLoc);
+            const startLoc = getSystem($start.text());
+            const endLoc = getSystem($end.text());
+            const route = calculateRoute(startLoc, endLoc);
 
-            var distance = 0;
-            var numJumps = 0;
-            var title = '';
-            for (var i = 0; i < route.length; i++) {
+            let distance = 0;
+            let numJumps = 0;
+            let title = '';
+            for (let i = 0; i < route.length; i++) {
                 distance += route[i].distance;
                 numJumps ++;
                 if (i === 0) {
@@ -131,10 +133,10 @@ var CorpCourierContracts = (function($, undefined) {
             }
             title += distance + ' ly.';
 
-        	var volumeShare     = volume / bltMaxVolume;
-        	var collateralShare = collateral / bltMaxCollat;
+            const volumeShare     = volume / bltMaxVolume;
+            const collateralShare = collateral / bltMaxCollateral;
 
-        	var calcReward = Math.round(baseFee + (
+            const calcReward = Math.round(baseFee + (
     			(Math.ceil(Math.max(volumeShare, collateralShare, 0.01) * 1000) / 1000) *
     			((distance * distanceFee) + (cynoFee * numJumps))
 			));
@@ -145,10 +147,19 @@ var CorpCourierContracts = (function($, undefined) {
         });
     }
 
+    function calcRewardM3() {
+        $('.outstanding tbody tr').each(function() {
+            const $row = $(this);
+            const volume = parseFloat($row.find('.volume').data('value'));
+            const reward = parseFloat($row.find('.reward').data('value'));
+            $row.find('.reward-m3').html(Math.round(reward / volume).toLocaleString('en-GB'));
+        });
+    }
+
+    // noinspection JSUnusedGlobalSymbols
     return {
 
         ready: function() {
-
             $.getJSON("/vendor/eve-map/systems.json", function(data) {
                 Route.graph = data;
                 calcReward();
@@ -156,26 +167,36 @@ var CorpCourierContracts = (function($, undefined) {
 
             calcSums('outstanding');
             calcSums('other');
+            calcRewardM3();
 
+            // noinspection JSUnusedGlobalSymbols
             $('.corp-courier-contracts .table').DataTable({
                 "aaSorting" : [], // no initial sorting
                 "paging" : false,
                 "info" : false,
                 "orderCellsTop": true,
                 fixedHeader : true,
+                columnDefs: [{
+                    targets: 'no-sort',
+                    orderable: false,
+                }],
                 initComplete: function() {
-                	var cols = this.hasClass('other') ? [0, 1, 2, 8, 9, 10] : [0, 1, 7, 8];
+                    const cols = this.hasClass('other') ? [0, 1, 2, 8, 9, 10] : [0, 1, 8, 9];
                     this.api().columns(cols).every(function() {
-                        var column = this;
-                        var select = $('<br><select onclick="$.Event(event).stopPropagation();"></select>')
-                            .appendTo($(column.header()))
+                        const column = this;
+                        const $header = $(column.header());
+                        const $select = $('<br><select></select>')
+                            .appendTo($header)
                             .width('100%')
                             .on('change', function() {
                                 column.search($(this).val()).draw();
                             });
-                        select.append('<option value=""></option>');
+                        $select.append('<option value=""></option>');
                         column.data().unique().sort().each(function(d) {
-                            select.append('<option value="' + d + '">' + d + '</option>');
+                            $select.append('<option value="' + d + '">' + d + '</option>');
+                        });
+                        $header.find('select').on('click', function (evt) {
+                            $.Event(evt).stopPropagation();
                         });
                     });
                 }
@@ -183,7 +204,7 @@ var CorpCourierContracts = (function($, undefined) {
         },
 
         select: function(ele) {
-        	var table;
+        	let table;
             if ($(ele).closest('table').hasClass('outstanding')) {
             	table = 'outstanding';
             } else {
