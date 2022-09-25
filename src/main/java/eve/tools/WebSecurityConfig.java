110d25343a2922cd1996c9f5a9d1a39516c2cc73
Tian Khamez <tkhamez@gmail.com>
2022-09-25 13:39:02 +0200

    Replace deprecated WebSecurityConfigurerAdapter with SecurityFilterChain.

--- src/main/java/eve/tools/WebSecurityConfig.java
+++ src/main/java/eve/tools/WebSecurityConfig.java
@@ -1,44 +1,31 @@
 package eve.tools;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
-	@Configuration
-	@Order(1)
-	public static class EveConfigurationAdapter extends WebSecurityConfigurerAdapter {
-
-		public EveConfigurationAdapter() {
-			super();
-		}
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.antMatcher("/**")
-					.formLogin()
-					.loginPage("/login")
-					.defaultSuccessUrl("/")
-					.and()
-					.logout()
-					.logoutUrl("/logout")
-					.logoutSuccessUrl("/")
-					.and()
-					.exceptionHandling()
-					.accessDeniedPage("/login")
-			;
-		}
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/").permitAll();
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.antMatcher("/**")
+			.formLogin()
+			.loginPage("/login")
+			.defaultSuccessUrl("/")
+			.and()
+			.logout()
+			.logoutUrl("/logout")
+			.logoutSuccessUrl("/")
+			.and()
+			.exceptionHandling()
+			.accessDeniedPage("/login")
+		;
+		return http.build();
 	}
 }
